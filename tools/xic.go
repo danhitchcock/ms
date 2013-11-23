08815d9b50c82e35fefa6d812b2524500abbbb1d
Pieter K <pieter@kunselhof.be>
2013-11-23 14:03:20 +0100

    xic: added a new flag to use the new speedy read functions

--- tools/xic.go
+++ tools/xic.go
@@ -30,16 +30,18 @@ type MS struct {
 func main() {
 	var mz float64
 	var tol float64
+	var mem bool
 	flag.Float64Var(&mz, "mz", 0, "m/z to filter on")
 	flag.Float64Var(&tol, "tol", 0, "allowed mz tolerance, can be used with -mz")
+	flag.BoolVar(&mem, "m", false, "read all scans in memory for a speed gain")
 	flag.Parse()
 
 	for _, filename := range flag.Args() {
-		PrintXIC(filename, mz, tol)
+		PrintXIC(filename, mz, tol, mem)
 	}
 }
 
-func PrintXIC(fn string, mz float64, tol float64) {
+func PrintXIC(fn string, mz float64, tol float64, mem bool) {
 	info, ver := unthermo.ReadFileHeaders(fn)
 
 	rh := new(unthermo.RunHeader)
@@ -48,54 +50,62 @@ func PrintXIC(fn string, mz float64, tol float64) {
 	//For later conversion of frequency values to m/z, we need a ScanEvent
 	//for each Scan.
 	//The list of them starts an uint32 later than ScantrailerAddr
-	//the uint32 contains the number of ScanEvents, but we know this
-	//already through SampleInfo
-	pos := rh.ScantrailerAddr + 4
-
-	//the ScanEvents are of variable size and have no pointer to
-	//them, we need to read them all sequentially
 	nScans := uint64(rh.SampleInfo.LastScanNumber - rh.SampleInfo.FirstScanNumber + 1)
-	scanevents := make([]unthermo.ScanEvent, nScans)
-	for i := range scanevents {
-		pos = unthermo.ReadFile(fn, pos, ver, &scanevents[i])
+	scanevents := make(unthermo.Scanevents, nScans)
+	unthermo.ReadFileRange(fn, rh.ScantrailerAddr + 4, rh.ScanparamsAddr, ver, scanevents)
+	
+	//read all scanindexentries (for retention time) at once,
+	//this is probably the fastest
+	scanindexentries := make(unthermo.ScanIndexEntries, nScans)
+	unthermo.ReadFileRange(fn, rh.ScanindexAddr, rh.ScantrailerAddr, ver, scanindexentries)
+
+	//initialize the shared data structure
+	
+	if mem {
+		//create channel to share memory with library
+		ch := make(chan *unthermo.ScanDataPacket)
+		//send off library to wait for work
+		go unthermo.ReadScansFromMemory(fn, rh.DataAddr, rh.OwnAddr, 0, ch)
+	
+		for s:=uint64(0); s<nScans ; s++ {
+			scan := new(unthermo.ScanDataPacket)
+			ch <- scan 	//send pointer to data structure
+			scan = <-ch //receive pointer back when library is done
+			PrintMaxInt(scan, &scanevents[s], &scanindexentries[s], mz, tol)
+		}
+	} else {
+		for s := range scanindexentries {
+			scan := new(unthermo.ScanDataPacket)
+			unthermo.ReadFile(fn, rh.DataAddr+scanindexentries[s].Offset, 0, scan)
+			PrintMaxInt(scan, &scanevents[s], &scanindexentries[s], mz, tol)
+		}
 	}
+}
 
-	//read all scanindexentries at once, this is probably the fastest
-	scanindexentries := make([]unthermo.ScanIndexEntry, nScans)
-	for i := range scanindexentries {
-		unthermo.ReadFile(fn,
-			rh.ScanindexAddr+uint64(i)*scanindexentries[i].Size(ver),
-			ver, &scanindexentries[i])
-	}
+func PrintMaxInt(scan *unthermo.ScanDataPacket, scanevent *unthermo.ScanEvent, sie *unthermo.ScanIndexEntry, mz float64, tol float64) {
+	var m []MS
 
-	for s := range scanindexentries {
-		scan := new(unthermo.ScanDataPacket)
-		unthermo.ReadFile(fn, rh.DataAddr+scanindexentries[s].Offset, 0, scan)
-
-		var m []MS
-
-		//convert the Hz values into m/z and save the signals within range
-		for i := uint32(0); i < scan.Profile.PeakCount; i++ {
-			for j := uint32(0); j < scan.Profile.Chunks[i].Nbins; j++ {
-				tmpmz := scanevents[s].Convert(scan.Profile.FirstValue+
-					float64(scan.Profile.Chunks[i].Firstbin+j)*scan.Profile.Step) +
-					float64(scan.Profile.Chunks[i].Fudge)
-				if tmpmz <= mz+tol && tmpmz >= mz-tol {
-					m = append(m, MS{tmpmz,
-						scan.Profile.Chunks[i].Signal[j]})
-				}
+	//convert Hz values into m/z and save the signals within range
+	for i := uint32(0); i < scan.Profile.PeakCount; i++ {
+		for j := uint32(0); j < scan.Profile.Chunks[i].Nbins; j++ {
+			tmpmz := scanevent.Convert(scan.Profile.FirstValue+
+				float64(scan.Profile.Chunks[i].Firstbin+j)*scan.Profile.Step) +
+				float64(scan.Profile.Chunks[i].Fudge)
+			if tmpmz <= mz+tol && tmpmz >= mz-tol {
+				m = append(m, MS{tmpmz,
+					scan.Profile.Chunks[i].Signal[j]})
 			}
 		}
+	}
 
-		//print the maximum signal
-		if len(m) > 0 {
-			var maxInt float32
-			for i := range m {
-				if m[i].I > maxInt {
-					maxInt = m[i].I
-				}
+	//print the maximum signal
+	if len(m) > 0 {
+		var maxIn MS
+		for i := range m {
+			if m[i].I > maxIn.I {
+				maxIn = m[i]
 			}
-			fmt.Println(scanindexentries[s].ScanTime, maxInt)
 		}
+		fmt.Println(sie.Index, maxIn.mz, maxIn.I)
 	}
 }
