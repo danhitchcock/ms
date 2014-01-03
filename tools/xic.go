00675b36cc4f2cdef255faa0422f7d9593bccf93
pkelchte <pieter@kunselhof.be>
2014-01-03 16:26:59 +0100

    xic: update documentation

--- tools/xic.go
+++ tools/xic.go
@@ -1,27 +1,36 @@
-//XIC prints a "chromatogram" for one ion.
-//
-//It prints the peak with highest intensity in interval [mz-tol,mz+tol]
-//for every profile-mode scan.
-//
-//Every line contains the retention time and intensity of a peak
-//
-//Example:
-//		xic -mz 361.1466 -tol 0.0025 rawfile.raw
-//
-//Output:
-//		0.003496666666666667 10500.583
-//		0.015028333333333333 11793.04
-//		0.03391333333333333 10178.598
-//		0.05393333333333334 10671.821
-//		0.07350833333333334 11572.251
+/*XIC prints mass chromatograms for given m/z's.
+
+  For every m/z given, it prints the peak with highest intensity in interval
+  [mz-tol ppm,mz+tol ppm] for every profile-mode scan.
+
+  Every line contains the mass, retention time and intensity of a peak
+
+  Reading can be sped up on systems with large memory by loading all scans
+  in RAM. Add flag -m on the command line for this.
+
+  Example:
+      xic -m -mz 361.1466 -mz 445.1200 -tol 2.5 rawfile.raw
+
+  Output:
+      361.1466 0.003496666666666667 10500.583
+      445.12 0.003496666666666667 37872.473
+      361.1466 0.015028333333333333 11793.04
+      445.12 0.015028333333333333 41592.734
+      361.1466 0.03391333333333333 10178.598
+      445.12 0.03391333333333333 38692.445
+      361.1466 0.05393333333333334 10671.821
+      445.12 0.05393333333333334 37769.496
+      361.1466 0.07350833333333334 11572.251
+      445.12 0.07350833333333334 37978.258
+*/
 package main
 
 import (
 	"bitbucket.org/proteinspector/unthermo"
 	"flag"
 	"fmt"
-	"strings"
 	"strconv"
+	"strings"
 )
 
 type MS struct {
@@ -38,13 +47,13 @@ func (i *mzarg) String() string {
 
 func (i *mzarg) Set(value string) error {
 	for _, splv := range strings.Split(value, ",") {
-		tmp, err := strconv.ParseFloat(splv,64)
+		tmp, err := strconv.ParseFloat(splv, 64)
 		if err != nil {
 			return err
 		}
 		*i = append(*i, tmp)
 	}
-    return nil
+	return nil
 }
 
 func main() {
@@ -72,8 +81,8 @@ func XIC(fn string, mz mzarg, tol float64, mem bool) {
 	//The list of them starts an uint32 later than ScantrailerAddr
 	nScans := uint64(rh.SampleInfo.LastScanNumber - rh.SampleInfo.FirstScanNumber + 1)
 	scanevents := make(unthermo.Scanevents, nScans)
-	unthermo.ReadFileRange(fn, rh.ScantrailerAddr + 4, rh.ScanparamsAddr, ver, scanevents)
-	
+	unthermo.ReadFileRange(fn, rh.ScantrailerAddr+4, rh.ScanparamsAddr, ver, scanevents)
+
 	//read all scanindexentries (for retention time) at once,
 	//this is probably the fastest
 	scanindexentries := make(unthermo.ScanIndexEntries, nScans)
@@ -86,12 +95,11 @@ func XIC(fn string, mz mzarg, tol float64, mem bool) {
 
 		//send off library to wait for work
 		go unthermo.ReadScansFromMemory(fn, rh.DataAddr, rh.OwnAddr, 0, offset, scans)
-		
 
-		for i,s := range scanindexentries {
+		for i, s := range scanindexentries {
 			offset <- s.Offset //send location of data structure
-			scan := <-scans //receive pointer back when library is done
-			for _,mz := range mz {
+			scan := <-scans    //receive pointer back when library is done
+			for _, mz := range mz {
 				PrintMaxPeak(scan, &scanevents[i], &s, mz, tol)
 			}
 		}
@@ -99,7 +107,7 @@ func XIC(fn string, mz mzarg, tol float64, mem bool) {
 		for i := range scanindexentries {
 			scan := new(unthermo.ScanDataPacket)
 			unthermo.ReadFile(fn, rh.DataAddr+scanindexentries[i].Offset, 0, scan)
-			for _,mz := range mz {
+			for _, mz := range mz {
 				PrintMaxPeak(scan, &scanevents[i], &scanindexentries[i], mz, tol)
 			}
 		}
