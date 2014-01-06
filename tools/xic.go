b8cc24ef3504447dcc94a8b9835e22b36b7bb0f1
pkelchte <pieter@kunselhof.be>
2014-01-06 17:27:26 +0100

    just replace xic with newer simple version instead of provide both

--- tools/xic.go
+++ tools/xic.go
@@ -1,7 +1,7 @@
 /*XIC prints mass chromatograms for given m/z's.
 
   For every m/z given, it prints the peak with highest intensity in interval
-  [mz-tol ppm,mz+tol ppm] for every profile-mode scan.
+  [mz-tol ppm,mz+tol ppm] for every MS-1 scan.
 
   Every line contains the mass, retention time and intensity of a peak
 
@@ -33,13 +33,14 @@ import (
 	"strings"
 )
 
-type MS struct {
-	mz float64
-	I  float32
-}
+/*
+ * Argument parsing (multiple m/z require a lot of boilerplate)
+ */
+type mzarg []float64
 
-//argument parsing
-type mzarg []float64 //a new type for passing flags on the command line
+var mzs mzarg
+var tol float64
+var mem bool
 
 func (i *mzarg) String() string {
 	return fmt.Sprintf("%d", *i)
@@ -56,88 +57,44 @@ func (i *mzarg) Set(value string) error {
 	return nil
 }
 
-func main() {
-	var mz mzarg
-	var tol float64
-	var mem bool
-	flag.Var(&mz, "mz", "m/z to filter on, may be specified multiple times")
+func init() {
+	flag.Var(&mzs, "mz", "m/z to filter on, this flag may be specified multiple times")
 	flag.Float64Var(&tol, "tol", 0, "allowed m/z tolerance in ppm, can be used with -mz")
 	flag.BoolVar(&mem, "m", false, "read all scans in memory for a speed gain")
 	flag.Parse()
+}
 
+/*
+ * Actual execution
+ */
+func main() {
 	for _, filename := range flag.Args() {
-		XIC(filename, mz, tol, mem)
+		//xic gets called on each MS1 Scan read by the unthermo library
+		unthermo.AllScans(filename, mem, 1, xic)
 	}
 }
 
-func XIC(fn string, mz mzarg, tol float64, mem bool) {
-	//Read necessary headers
-	info, ver := unthermo.ReadFileHeaders(fn)
-	rh := new(unthermo.RunHeader)
-	unthermo.ReadFile(fn, info.Preamble.RunHeaderAddr[0], ver, rh)
-
-	//For later conversion of frequency values to m/z, we need a ScanEvent
-	//for each Scan.
-	//The list of them starts an uint32 later than ScantrailerAddr
-	nScans := uint64(rh.SampleInfo.LastScanNumber - rh.SampleInfo.FirstScanNumber + 1)
-	scanevents := make(unthermo.Scanevents, nScans)
-	unthermo.ReadFileRange(fn, rh.ScantrailerAddr+4, rh.ScanparamsAddr, ver, scanevents)
-
-	//read all scanindexentries (for retention time) at once,
-	//this is probably the fastest
-	scanindexentries := make(unthermo.ScanIndexEntries, nScans)
-	unthermo.ReadFileRange(fn, rh.ScanindexAddr, rh.ScantrailerAddr, ver, scanindexentries)
-
-	if mem {
-		//create channels to share memory with library
-		offset := make(chan uint64)
-		scans := make(chan *unthermo.ScanDataPacket)
-
-		//send off library to wait for work
-		go unthermo.ReadScansFromMemory(fn, rh.DataAddr, rh.OwnAddr, 0, offset, scans)
-
-		for i, s := range scanindexentries {
-			offset <- s.Offset //send location of data structure
-			scan := <-scans    //receive pointer back when library is done
-			for _, mz := range mz {
-				PrintMaxPeak(scan, &scanevents[i], &s, mz, tol)
+//Algorithm calculating and outputting the peaks belonging to the XIC's
+var xic = func(spectrum unthermo.Spectrum) {
+	//for every mz in the argument list
+	for _, mz := range mzs {
+		//save the spectrum within range
+		var specWithinTol unthermo.Spectrum
+		for _, peak := range spectrum {
+			if peak.Mz <= mz+10e-6*tol*mz && peak.Mz >= mz-10e-6*tol*mz {
+				specWithinTol = append(specWithinTol, peak)
 			}
 		}
-	} else {
-		for i := range scanindexentries {
-			scan := new(unthermo.ScanDataPacket)
-			unthermo.ReadFile(fn, rh.DataAddr+scanindexentries[i].Offset, 0, scan)
-			for _, mz := range mz {
-				PrintMaxPeak(scan, &scanevents[i], &scanindexentries[i], mz, tol)
-			}
-		}
-	}
-}
 
-func PrintMaxPeak(scan *unthermo.ScanDataPacket, scanevent *unthermo.ScanEvent, sie *unthermo.ScanIndexEntry, mz float64, tol float64) {
-	var m []MS
-
-	//convert Hz values into m/z and save the signals within range
-	for i := uint32(0); i < scan.Profile.PeakCount; i++ {
-		for j := uint32(0); j < scan.Profile.Chunks[i].Nbins; j++ {
-			tmpmz := scanevent.Convert(scan.Profile.FirstValue+
-				float64(scan.Profile.Chunks[i].Firstbin+j)*scan.Profile.Step) +
-				float64(scan.Profile.Chunks[i].Fudge)
-			if tmpmz <= mz+10e-6*tol*mz && tmpmz >= mz-10e-6*tol*mz {
-				m = append(m, MS{tmpmz,
-					scan.Profile.Chunks[i].Signal[j]})
+		//print the maximum signal of specWithinTol
+		if len(specWithinTol) > 0 {
+			var maxIn unthermo.Peak
+			for _, peak := range specWithinTol {
+				if peak.I > maxIn.I {
+					maxIn = peak
+				}
 			}
+			fmt.Println(mz, maxIn.Time, maxIn.I)
 		}
-	}
-
-	//print the maximum signal
-	if len(m) > 0 {
-		var maxIn MS
-		for i := range m {
-			if m[i].I > maxIn.I {
-				maxIn = m[i]
-			}
-		}
-		fmt.Println(mz, sie.Time, maxIn.I)
 	}
 }
