c704e51924b2febe62cbae6170474824aba1648b
pkelchte <pieter@kunselhof.be>
2014-03-13 15:14:44 +0100

    added more documentation to ms package

--- data.go
+++ data.go
@@ -1,13 +1,16 @@
-//A library for mass spectrometry data
+//Package ms is a library for mass spectrometry data
 package ms
 
+//An ion peak
 type Peak struct {
 	Mz   float64
 	I    float32
 }
 
+//A collection of peaks
 type Spectrum []Peak
 
+//The scan event of the mass spectrometer
 type Scan struct {
 	Activation Activation
 	MSLevel    uint8
@@ -15,10 +18,13 @@ type Scan struct {
 	Time float64
 }
 
+//For fragmentation scans, there is an activation type
 type Activation int
 
 const (
+	//Collision induced dissociation
 	CID Activation = iota
+	//Higher-energy collisional dissociation
 	HCD
 )
 
